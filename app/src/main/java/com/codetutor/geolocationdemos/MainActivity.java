e260fd3e64f32881ff8c048bc1bf053daf240f2e
Anil Deshpande <anildesh82@gmail.com>
2019-09-12 13:09:59 +0530

    first commit
    This will be the demo app on Codetutor for Location based tutorial
    videos

---
+++ app/src/main/java/com/codetutor/geolocationdemos/MainActivity.java
@@ -0,0 +1,137 @@
+package com.codetutor.geolocationdemos;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationAvailability;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private static final int UPDATE_INTERVAL = 5000; // 5 seconds
+
+    FusedLocationProviderClient locationProviderClient;
+    LocationRequest locationRequest;
+    LocationCallback locationCallback;
+    private Location currentLocation;
+    private int LOCATION_PERMISSION = 100;
+
+    Button buttonGetLocation, buttonStopLocation;
+    TextView textViewLatitude, textViewLongitude;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonGetLocation = (Button)findViewById(R.id.buttonGetLocation);
+        buttonStopLocation = (Button)findViewById(R.id.buttonStopLocation);
+
+        textViewLatitude = (TextView)findViewById(R.id.textViewLatitude);
+        textViewLongitude = (TextView)findViewById(R.id.textViewLongitude);
+
+        buttonGetLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startGettingLocation();
+            }
+        });
+
+        buttonStopLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stopLocationRequests();
+            }
+        });
+
+        locationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(UPDATE_INTERVAL);
+        locationCallback = new LocationCallback(){
+            @Override
+            public void onLocationAvailability(LocationAvailability locationAvailability) {
+                super.onLocationAvailability(locationAvailability);
+                if (locationAvailability.isLocationAvailable()){
+                    Log.i(TAG,"Location is available");
+                }else {
+                    Log.i(TAG,"Location is unavailable");
+                }
+
+            }
+
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+                Log.i(TAG,"Location result is available");
+            }
+        };
+    }
+
+    private void startGettingLocation() {
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)== PackageManager.PERMISSION_GRANTED &&
+           ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)== PackageManager.PERMISSION_GRANTED){
+            locationProviderClient.requestLocationUpdates(locationRequest, locationCallback, MainActivity.this.getMainLooper());
+            locationProviderClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    currentLocation  = location;
+                    textViewLatitude.setText("Latitude: "+currentLocation.getLatitude());
+                    textViewLongitude.setText("Longitude: "+currentLocation.getLongitude());
+                }
+            });
+
+            locationProviderClient.getLastLocation().addOnFailureListener(this, new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Log.i(TAG,"Exception while getting the location: "+e.getMessage());
+                }
+            });
+
+        }else {
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)){
+                Toast.makeText(MainActivity.this, "Permission needed", Toast.LENGTH_LONG).show();
+            } else {
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[] {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
+                        LOCATION_PERMISSION);
+            }
+        }
+    }
+
+    private void stopLocationRequests(){
+        locationProviderClient.removeLocationUpdates(locationCallback);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        startGettingLocation();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        stopLocationRequests();
+    }
+}
